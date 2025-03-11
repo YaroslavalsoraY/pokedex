0007b8d392bcd003eea0e2577488d4541f257a3d
YaroslavalsoraY <jaroslavpodoprigora@yandex.ru>
2025-03-11 14:34:53 +0300

    add map

--- main.go
+++ main.go
@@ -7,14 +7,22 @@ import (
 )
 
 func main() {
+	first_adress := "https://pokeapi.co/api/v2/location-area?offset=0&limit=20"
+	conf := config{
+		next: &first_adress,
+		previous: nil,
+	}
 	commandsDB := getCommands()
+	fmt.Print("Pokedex > ")
 	sc := bufio.NewScanner(os.Stdin)
 	for sc.Scan() {
 		_, ok := commandsDB[sc.Text()]
 		if !ok {
 			fmt.Println("Unknown command")
+			fmt.Print("Pokedex > ")
 		} else {
-			commandsDB[sc.Text()].callback()
+			commandsDB[sc.Text()].callback(&conf)
+			fmt.Print("Pokedex > ")
 		}
 	}
 }
--- repl.go
+++ repl.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
+	"io"
+	"net/http"
 	"os"
 	"strings"
 )
@@ -21,19 +25,89 @@ func cleanInput(text string) []string {
 	return result
 }
 
-func commandExit() error {
+func commandExit(c *config) error {
 	fmt.Println("Closing the Pokedex... Goodbye!")
 	os.Exit(0)
 	return nil
 }
 
-func commandHelp() error {
+func commandHelp(c *config) error {
 	commandsDB := getCommands()
 	fmt.Print("Welcome to the Pokedex!\nUsage:\n\n")
 	for _, v := range commandsDB {
 		fmt.Printf("%s: %s\n", v.name, v.decsription)
 	}
 	return nil
+}
+
+func commandMap(c *config) error {
+	type JSONresp struct {
+		Count    int     `json:"count"`
+		Next     *string `json:"next"`
+		Previous *string `json:"previous"`
+		Results  []struct {
+			Name string `json:"name"`
+			URL  string `json:"url"`
+		} `json:"results"`
+	}
+	resp, err := http.Get(*c.next)
+	if err != nil {
+		return errors.New("error with connection")
+	}
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return errors.New("problem with reading")
+	}
+	locations := JSONresp{}
+	defer resp.Body.Close()
+	err = json.Unmarshal(body, &locations)
+	if err != nil {
+		return errors.New("Unmarshalling error")
+	}
+	for _, loc := range locations.Results {
+		fmt.Println(loc.Name)
+	}
+	c.next = locations.Next
+	c.previous = locations.Previous
+	return nil
+}
+
+func commandMapb(c *config) error {
+	if c.previous == nil {
+		fmt.Println("you're on the first page")
+		return nil
+	}
+
+	type JSONresp struct {
+		Count    int     `json:"count"`
+		Next     *string `json:"next"`
+		Previous *string `json:"previous"`
+		Results  []struct {
+			Name string `json:"name"`
+			URL  string `json:"url"`
+		} `json:"results"`
+	}
+	resp, err := http.Get(*c.previous)
+	if err != nil {
+		return errors.New("error with connection")
+	}
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return errors.New("problem with reading")
+	}
+	locations := JSONresp{}
+	defer resp.Body.Close()
+	err = json.Unmarshal(body, &locations)
+	if err != nil {
+		return errors.New("Unmarshalling error")
+	}
+	for _, loc := range locations.Results {
+		fmt.Println(loc.Name)
+	}
+	c.next = locations.Next
+	c.previous = locations.Previous
+	return nil
+
 }
 
 func getCommands() map[string]cliCommand {
@@ -45,8 +119,18 @@ func getCommands() map[string]cliCommand {
 		},
 		"help": {
 			name:        "help",
-			decsription: "commands manual",
+			decsription: "Commands manual",
 			callback:    commandHelp,
+		},
+		"map": {
+			name:        "map",
+			decsription: "Show locations",
+			callback:    commandMap,
+		},
+		"mapb": {
+			name: "mapb",
+			decsription: "Show previos locations",
+			callback: commandMapb,
 		},
 	}
 }
@@ -54,5 +138,10 @@ func getCommands() map[string]cliCommand {
 type cliCommand struct {
 	name        string
 	decsription string
-	callback    func() error
+	callback    func(c *config) error
+}
+
+type config struct {
+	next     *string
+	previous *string
 }
